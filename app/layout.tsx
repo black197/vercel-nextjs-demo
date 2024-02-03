7b5bcbe4bd3123c7a88f7782357bbc9eff1b2707
DennisYeh <dyeh68306@163.com>
2024-02-04 01:22:00 +0800

    feat: import mui font css

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,10 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
